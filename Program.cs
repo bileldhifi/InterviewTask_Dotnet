0b175a8716e5f7e2084e2d7d10c14d799a2b0562
Bilel DHIFI <bileldhifi37@gmail.com>
2025-06-19 22:30:35 +0200

    feat: setup Cors

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 
 builder.Services.AddControllers(); 
 builder.Services.AddEndpointsApiExplorer(); 
@@ -22,7 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();      
     app.UseSwaggerUI();    
 }
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();    
